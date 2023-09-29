ae9381f4e8e57e2899e5fef07ad11e34aa2aed6a
Jamie Skinner <jamie@etcetera.cr>
2023-09-29 17:32:02 -0500

    first frame of 1 kinect, 1 live

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ from fastapi.responses import FileResponse
 
 from freenect2 import Device, FrameType
 
-undistorted_depth1 = []
-# undistorted_depth2 = np.array([])
+undistorted_depth1 = np.array([])
+undistorted_depth2 = np.array([])
 frames = {}
 frames2 = {}
 payload = []
@@ -27,7 +27,7 @@ app.mount("/dist", StaticFiles(directory="dist"), name="dist")
 logging.info("SErver running")
 
 device = Device(serial=b'088079340147')
-# device2 = Device(serial=b'032351734147')
+device2 = Device(serial=b'032351734147')
 
 
 def process_depth(data2d, skip=2):
@@ -52,15 +52,15 @@ async def capture_frames():
         await asyncio.sleep(0.01)  # This sleep is to prevent the loop from being too busy
 
 
-# def capture_frames2():
-#     global undistorted_depth2
-#     with device2.running():
-#         for type_, frame2 in device2:
-#             frames2[type_] = frame2
-#             # Capture undistorted_depth and registered_color frames
-#             if FrameType.Depth in frames2:
-#                 # Process and store the frames as needed
-#                 undistorted_depth2 = frames2[FrameType.Depth]
+def capture_frames2():
+    global undistorted_depth2
+    with device2.running():
+        for type_, frame2 in device2:
+            frames2[type_] = frame2
+            # Capture undistorted_depth and registered_color frames
+            if FrameType.Depth in frames2:
+                # Process and store the frames as needed
+                undistorted_depth2 = frames2[FrameType.Depth]
 
 
 
@@ -69,13 +69,22 @@ def capture_frames_thread():
     asyncio.set_event_loop(loop)
     loop.run_until_complete(capture_frames())
 
-
-
 # Start the frame capture loop in a separate thread
 capture_thread = threading.Thread(target=capture_frames_thread)
 # Allow the thread to be terminated when the main program exits
 capture_thread.daemon = True
 capture_thread.start()
+
+def capture_frames_thread2():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(capture_frames2())
+
+# Start the frame capture loop in a separate thread
+capture_thread2 = threading.Thread(target=capture_frames_thread2)
+# Allow the thread to be terminated when the main program exits
+capture_thread2.daemon = True
+capture_thread2.start()
 
 
 @app.get("/")
@@ -93,7 +102,8 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             result = []
-            for camera in [undistorted_depth1]:
+            for camera in [undistorted_depth1, undistorted_depth2]:
+                logging.info("are they equal", str(undistorted_depth2 == undistorted_depth1))
                 try:
                     depth_array = camera.to_array()
                     depth_processed = process_depth(depth_array)
